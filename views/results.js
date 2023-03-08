52aacaca9cc4e99d602df0fe0ec4b49df43cd522
Sophia Ho <sophiahomy@gmail.com>
2023-03-08 12:00:33 +0000

    Fetch result until solved

--- views/results.js
+++ views/results.js
@@ -2,13 +2,30 @@ var url = new URL(document.location);
 var urlParams = url.searchParams;
 var id = urlParams.get("id")
 
-fetch("getJob", {
-    method: 'POST', headers: {
-        'Content-Type': 'application/json'
-    }, body: JSON.stringify({
-        data: id,
-    })
-}).then(response => response.json())
-    .then(json => {
-        document.getElementById("result-container").textContent = JSON.stringify(json, undefined, 2);
-})
+getResult();
+
+function getResult() {
+    var solverDone = false;
+
+    if(!solverDone) {
+        fetch("getJob", {
+            method: 'POST', headers: {
+                'Content-Type': 'application/json'
+            }, body: JSON.stringify({
+                data: id,
+            })
+        }).then(response => response.json())
+            .then(json => {
+                if(json.status == "wait") {
+                    setTimeout(getResult, 2000);
+                    document.getElementById("result-container").textContent = "Loading";
+                }
+                else {
+                    solverDone = true;
+                    document.getElementById("result-container").textContent = JSON.stringify(json, undefined, 2);
+                }
+               
+        })
+    }
+    
+}
