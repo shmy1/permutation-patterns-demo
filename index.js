145212f1b3630950f3ead36ef8b9dfa1201e6122
Sophia Ho <sophiahomy@gmail.com>
2023-03-27 13:58:16 +0000

    Automatically reload page when going back

--- index.js
+++ index.js
@@ -565,3 +565,11 @@ function isValidLength(patterns, length) {
 
 
 }
+
+window.addEventListener( "pageshow", function ( event ) {
+    var perfEntries = performance.getEntriesByType("navigation");
+
+if (perfEntries[0].type === "back_forward") {
+    location.reload();
+}
+  });
